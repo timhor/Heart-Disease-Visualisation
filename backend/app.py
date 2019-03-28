03c5078641d2f6ac505791fb410848d2ae2e7a2e
Mitchell <mitchell_shelton@y7mail.com>
2019-03-28 15:46:40 +1100

    added before_request function to load data. Still needs to be completed

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,10 @@ from flask import Flask
 from .stats import views as stats_views
 from .views import base
 
+def load_data():
+    # TODO
+    print("Loading data")
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -20,6 +24,8 @@ def create_app(test_config=None):
 
     # Blueprint for our /stats route
     app.register_blueprint(stats_views.bp)
+
+    app.before_request(load_data)
     
     return app
 
