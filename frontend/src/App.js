4298e61987104eab0a7e6fdc1aeb88437e24aabd
Tim Hor <timothy.hor@outlook.com>
2019-04-01 15:58:45 +1100

    Add sample plot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Plot from 'react-plotly.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Plot
+        data={[
+          {
+            name: 'line chart',
+            type: 'scatter',
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            mode: 'lines+points',
+            marker: { color: 'red' }
+          },
+          {
+            name: 'bar chart',
+            type: 'bar',
+            x: [1, 2, 3], y: [2, 5, 3]
+          }
+        ]}
+        layout={
+          {
+            title: 'Sample Plot',
+            width: 800,
+            height: 500
+          }
+        }
+      />
     );
   }
 }
