255c30c94a2df8f836e916d33c4e745e690cd22e
Mitchell <mitchell_shelton@y7mail.com>
2019-04-02 14:58:42 +1100

    completed /stats and /stats/{stat} routes

--- backend/stats/views.py
+++ backend/stats/views.py
@@ -6,14 +6,21 @@ bp = Blueprint('stats', __name__, url_prefix='/stats')
 @bp.route('/', methods=['GET'])
 def get_stats():
     '''Get all the stats'''
-    # TODO
-    return "GET ALL STATS"
+    if 'df' not in g:
+        return "Dataframe has not been loaded", 404
+
+    return jsonify(g.df.to_json(orient='records'))
 
 @bp.route('/<stat>', methods=['GET'])
 def get_stat(stat):
     '''Get a specific stat'''
-    # TODO
-    return "GET STAT " + stat
+    if 'df' not in g:
+        return "Dataframe has not been loaded", 404
+  
+    if stat not in g.df:
+        return f"Invalid stat {stat} specified", 400
+
+    return jsonify(g.df[stat].to_json(orient='records'))
 
 # TODO
 # Route that accepts user input and does machine learning
