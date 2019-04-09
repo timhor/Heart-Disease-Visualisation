b713c51fbbf15fac78ec840c361e4d74e57fa406
Mitchell <mitchell_shelton@y7mail.com>
2019-04-09 15:13:22 +1000

    spacing between header buttons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -17,26 +17,31 @@ class Header extends Component {
         return (
             <AppBar position="static" color="default" >
                 <Toolbar>
-                <Typography variant="h6" color="inherit"  >
-                    ANTHEM
-                </Typography>
-                <Grid
-                    container
-                    direction="row"
-                    justify="flex-end"
-                    alignItems="center"
-                >
-                    <Button color="primary" variant="outlined">
-                    <Link to="/plots" style={{ textDecoration: 'none' }}>
-                        Plots
-                    </Link>
-                    </Button>
-                    <Button>
-                    <Link to="/prediction" style={{ textDecoration: 'none' }}>
-                        Prediction
-                    </Link>
-                    </Button>
-                </Grid>
+                    <Typography variant="h6" color="inherit"  >
+                        ANTHEM
+                    </Typography>
+                    <Grid
+                        container
+                        direction="row"
+                        justify="flex-end"
+                        alignItems="center"
+                        spacing={8}
+                    >
+                        <Grid item> 
+                            <Button color="primary" variant="outlined">
+                                <Link to="/plots" style={{ textDecoration: 'none' }}>
+                                    Plots
+                                </Link>
+                            </Button>
+                        </Grid>
+                        <Grid item >
+                            <Button color="secondary">
+                                <Link to="/prediction" style={{ textDecoration: 'none' }}>
+                                    Prediction
+                                </Link>
+                            </Button>
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </AppBar>
         );
