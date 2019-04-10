174c0a9a848410a38cbdcd480f7997e56432c326
Tim Hor <timothy.hor@outlook.com>
2019-04-10 15:55:49 +1000

    Format code in header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,51 +1,42 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Link } from 'react-router-dom';
-import {
-  Button,
-  AppBar,
-  Toolbar,
-  Typography,
-  Grid
-} from '@material-ui/core';
+import { Button, AppBar, Toolbar, Typography, Grid } from '@material-ui/core';
 import '../css/App.css';
 
-
 class Header extends Component {
-
-    render() {
-  
-        return (
-            <AppBar position="static" color="default" >
-                <Toolbar>
-                    <Typography variant="h6" color="inherit"  >
-                        ANTHEM
-                    </Typography>
-                    <Grid
-                        container
-                        direction="row"
-                        justify="flex-end"
-                        alignItems="center"
-                        spacing={8}
-                    >
-                        <Grid item> 
-                            <Button color="primary" variant="outlined">
-                                <Link to="/plots" style={{ textDecoration: 'none' }}>
-                                    Plots
-                                </Link>
-                            </Button>
-                        </Grid>
-                        <Grid item >
-                            <Button color="secondary">
-                                <Link to="/prediction" style={{ textDecoration: 'none' }}>
-                                    Prediction
-                                </Link>
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </Toolbar>
-            </AppBar>
-        );
-    }
+  render() {
+    return (
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            ANTHEM
+          </Typography>
+          <Grid
+            container
+            direction="row"
+            justify="flex-end"
+            alignItems="center"
+            spacing={8}
+          >
+            <Grid item>
+              <Button color="primary" variant="outlined">
+                <Link to="/plots" style={{ textDecoration: 'none' }}>
+                  Plots
+                </Link>
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button color="secondary">
+                <Link to="/prediction" style={{ textDecoration: 'none' }}>
+                  Prediction
+                </Link>
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
 export default Header;
