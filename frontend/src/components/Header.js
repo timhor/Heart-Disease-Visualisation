dea94d15354fee2865cce3ee5596d367ac2bade2
Tim Hor <timothy.hor@outlook.com>
2019-04-10 15:54:47 +1000

    Remove unused imports

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Link } from 'react-router-dom';
 import {
   Button,
   AppBar,
--- frontend/src/components/Plots.js
+++ frontend/src/components/Plots.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import Plot from 'react-plotly.js';
 import backend from '../api/backend';
 import Header from './Header';
