fd88cfa0623651b45e0ae39dcffe3e33b5e4b11d
Tim Hor <timothy.hor@outlook.com>
2019-04-10 15:51:56 +1000

    Add buttons to pre-fill sample inputs

--- frontend/src/components/Prediction.js
+++ frontend/src/components/Prediction.js
@@ -39,7 +39,7 @@ class Prediction extends Component {
       .map(entry => entry.join('='))
       .join('&');
     const response = await backend.get('/prediction?' + queryString);
-    console.log(response);
+    this.setState({ prediction: response.data.target });
   }
 
   handleChange = name => event => {
@@ -52,6 +52,44 @@ class Prediction extends Component {
 
   resetState = () => {
     this.setState(initialState);
+  };
+
+  // no heart disease
+  prefillSample1 = () => {
+    this.setState({
+      age: '63',
+      sex: '1',
+      cp: '1',
+      trestbps: '145.0',
+      chol: '233.0',
+      fbs: '1',
+      restecg: '2',
+      thalach: '150.0',
+      exang: '0',
+      oldpeak: '2.3',
+      slope: '3',
+      ca: '0',
+      thal: '6'
+    });
+  };
+
+  // heart disease
+  prefillSample2 = () => {
+    this.setState({
+      age: '67',
+      sex: '1',
+      cp: '4',
+      trestbps: '160.0',
+      chol: '286.0',
+      fbs: '0',
+      restecg: '2',
+      thalach: '108.0',
+      exang: '1',
+      oldpeak: '1.5',
+      slope: '2',
+      ca: '3',
+      thal: '3'
+    });
   };
 
   render() {
@@ -74,8 +112,8 @@ class Prediction extends Component {
                 value={this.state.sex}
                 onChange={this.handleChange('sex')}
               >
-                <MenuItem value={0}>Female</MenuItem>
-                <MenuItem value={1}>Male</MenuItem>
+                <MenuItem value={'0'}>Female</MenuItem>
+                <MenuItem value={'1'}>Male</MenuItem>
               </Select>
             </FormControl>
 
@@ -86,10 +124,10 @@ class Prediction extends Component {
                 value={this.state.cp}
                 onChange={this.handleChange('cp')}
               >
-                <MenuItem value={1}>Typical anigma</MenuItem>
-                <MenuItem value={2}>Atypical angina</MenuItem>
-                <MenuItem value={3}>Non-anginal pain</MenuItem>
-                <MenuItem value={4}>Asymptomatic</MenuItem>
+                <MenuItem value={'1'}>Typical anigma</MenuItem>
+                <MenuItem value={'2'}>Atypical angina</MenuItem>
+                <MenuItem value={'3'}>Non-anginal pain</MenuItem>
+                <MenuItem value={'4'}>Asymptomatic</MenuItem>
               </Select>
             </FormControl>
 
@@ -114,8 +152,8 @@ class Prediction extends Component {
                 value={this.state.fbs}
                 onChange={this.handleChange('fbs')}
               >
-                <MenuItem value={0}>&le; 120mg/dl</MenuItem>
-                <MenuItem value={1}>&gt; 120mg/dl</MenuItem>
+                <MenuItem value={'0'}>&le; 120mg/dl</MenuItem>
+                <MenuItem value={'1'}>&gt; 120mg/dl</MenuItem>
               </Select>
             </FormControl>
 
@@ -126,9 +164,9 @@ class Prediction extends Component {
                 value={this.state.restecg}
                 onChange={this.handleChange('restecg')}
               >
-                <MenuItem value={0}>Normal</MenuItem>
-                <MenuItem value={1}>ST-T wave abnormality</MenuItem>
-                <MenuItem value={2}>Left ventricular hypertrophy</MenuItem>
+                <MenuItem value={'0'}>Normal</MenuItem>
+                <MenuItem value={'1'}>ST-T wave abnormality</MenuItem>
+                <MenuItem value={'2'}>Left ventricular hypertrophy</MenuItem>
               </Select>
             </FormControl>
 
@@ -140,14 +178,16 @@ class Prediction extends Component {
             />
 
             <FormControl>
-              <InputLabel htmlFor="exang">Exercise-induced angina</InputLabel>
+              <InputLabel htmlFor="exang">
+                Exercise-induced angina
+              </InputLabel>
               <Select
                 name="exang"
                 value={this.state.exang}
                 onChange={this.handleChange('exang')}
               >
-                <MenuItem value={0}>No</MenuItem>
-                <MenuItem value={1}>Yes</MenuItem>
+                <MenuItem value={'0'}>No</MenuItem>
+                <MenuItem value={'1'}>Yes</MenuItem>
               </Select>
             </FormControl>
 
@@ -159,15 +199,17 @@ class Prediction extends Component {
             />
 
             <FormControl>
-              <InputLabel htmlFor="slope">Slope of peak exercise ST segment</InputLabel>
+              <InputLabel htmlFor="slope">
+                Slope of peak exercise ST segment
+              </InputLabel>
               <Select
                 name="slope"
                 value={this.state.slope}
                 onChange={this.handleChange('slope')}
               >
-                <MenuItem value={1}>Up</MenuItem>
-                <MenuItem value={2}>Flat</MenuItem>
-                <MenuItem value={3}>Down</MenuItem>
+                <MenuItem value={'1'}>Up</MenuItem>
+                <MenuItem value={'2'}>Flat</MenuItem>
+                <MenuItem value={'3'}>Down</MenuItem>
               </Select>
             </FormControl>
 
@@ -185,13 +227,29 @@ class Prediction extends Component {
                 value={this.state.thal}
                 onChange={this.handleChange('thal')}
               >
-                <MenuItem value={3}>Normal</MenuItem>
-                <MenuItem value={6}>Fixed defect</MenuItem>
-                <MenuItem value={7}>Reversable defect</MenuItem>
+                <MenuItem value={'3'}>Normal</MenuItem>
+                <MenuItem value={'6'}>Fixed defect</MenuItem>
+                <MenuItem value={'7'}>Reversable defect</MenuItem>
               </Select>
             </FormControl>
           </div>
 
+          <Button
+            variant="contained"
+            color="default"
+            className="Button"
+            onClick={this.prefillSample1}
+          >
+            Pre-fill Sample 1
+          </Button>
+          <Button
+            variant="contained"
+            color="default"
+            className="Button"
+            onClick={this.prefillSample2}
+          >
+            Pre-fill Sample 2
+          </Button>
           <Button
             variant="contained"
             color="primary"
